56af8570905a9f4aeeb3d19cd1232eeca3cfbcef
Anas <mahdisomjee04@gmail.com>
2025-03-27 10:11:57 +0530

    Bug Fixes

--- AccuraLiveness_fm.xcframework/ios-arm64/AccuraLiveness_fm.framework/Headers/VideoCameraWrapperDelegate.h
+++ AccuraLiveness_fm.xcframework/ios-arm64/AccuraLiveness_fm.framework/Headers/VideoCameraWrapperDelegate.h
@@ -9,10 +9,17 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+typedef NS_ENUM(int, LivenessType)
+{
+    DEFAULT = 0, LOOK_LEFT = 1, LOOK_RIGHT = 2, ORAL_VERIFICATION = 3, APPROVED = 4
+};
 
 @protocol LivenessVideoCameraWrapperDelegate <NSObject>
 @optional
+-(void)didChangedLivenessType:(LivenessType)faceDirection;
 -(void)livenessData:(UIImage*)livenessImage andshowImage:(UIImage*)showImage;
+-(void)livenessData:(UIImage*)livenessImage andshowImage:(UIImage*)showImage imagePath:(NSString*)imagePath;
+-(void)getRandomNumber:(NSString*)randomNumber;
 -(void)processedImage:(UIImage*)image;
 -(void)titleMessage:(NSString *)message;
 
